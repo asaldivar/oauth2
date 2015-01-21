b0cad063804c12b41ffc9f78166094cdefa43c59
alexander <ag.saldivar@gmail.com>
2015-01-20 19:14:17 -0800

    create ability to ping Instagram oauth/authorize/ and print response body

--- oauth2.go
+++ oauth2.go
@@ -3,6 +3,8 @@ package main
 import (
   "fmt"
   "net/http"
+  "net/url"
+  "io/ioutil"
 
   "github.com/zenazn/goji"
   "github.com/zenazn/goji/web"
@@ -12,7 +14,43 @@ func home(c web.C, w http.ResponseWriter, r *http.Request) {
   fmt.Fprintf(w, "Hello, %s!", c.URLParams["name"])
 }
 
+const (
+  clientId     = "de761aa8f066479fb7ea069396ae50b5"
+  clientSecret = "0dd10b36d467450aaad644ce44e51028"
+  redirectURI  = "http://localhost:8000/hello/alex"
+)
+
+func auth(c web.C, w http.ResponseWriter, r *http.Request) {
+  // parse Instagram's authorize endpoint
+  authorizeEndpoint, _ := url.Parse("https://api.instagram.com/oauth/authorize/")
+
+  // create necessary params for endpoint
+  params := url.Values{}
+  params.Add("client_id", clientId)
+  params.Add("redirect_uri", redirectURI)
+  params.Add("response_type", "code")
+
+  // encode values into URL encoded form and append to endpoint
+  authorizeEndpoint.RawQuery = params.Encode()
+
+  // GET the endpoint
+  resp, err := http.Get(authorizeEndpoint.String())
+  if err != nil {
+    fmt.Println(err)
+  }
+  defer resp.Body.Close()
+  body, err := ioutil.ReadAll(resp.Body)
+  if err != nil {
+    fmt.Println(err)
+  }
+
+  // print the response body
+  fmt.Println(string(body))
+
+}
+
 func main() {
+  goji.Get("/instagram/auth", auth)
   goji.Get("/home/:name", home)
   goji.Serve()
 }
